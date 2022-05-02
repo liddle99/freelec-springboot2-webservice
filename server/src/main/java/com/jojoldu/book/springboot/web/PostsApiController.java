5796bf9e9636ef6f362d258352c2db9653778687
cocktail-Lewis <sunkyu.kim@cocktailfd.com>
2022-05-03 02:08:18 +0900

    feat: 추가,수정,제거 기능 추가

---
+++ server/src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -0,0 +1,23 @@
+//package com.jojoldu.book.springboot.web;
+//
+//
+//import com.jojoldu.book.springboot.service.posts.PostsService;
+//import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
+//import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
+//import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+//import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RequiredArgsConstructor
+//@RestController
+//@CrossOrigin(origins = "*", allowedHeaders = "*")
+//public class PostsApiController {
+//
+//    private final PostsService postsService;
+//
+//
+//}
