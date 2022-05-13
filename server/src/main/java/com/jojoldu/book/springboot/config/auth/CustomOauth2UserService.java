04a06169280ce9bd1dff34df597d4ba070becb2a
liddle99 <liddle9985@gmail.com>
2022-05-13 21:10:19 +0900

    Revert "feat: 구글 oauth 연동 진행중"
    This reverts commit bb5793656285cf51d2dd5ee3b7bda98ed6b60b9c.

--- server/src/main/java/com/jojoldu/book/springboot/config/auth/CustomOauth2UserService.java
+++ server/src/main/java/com/jojoldu/book/springboot/config/auth/CustomOauth2UserService.java
@@ -1,20 +1,15 @@
 package com.jojoldu.book.springboot.config.auth;
 
-import com.jojoldu.book.springboot.config.auth.dto.OAuthAttributes;
-import com.jojoldu.book.springboot.domain.user.User;
 import com.jojoldu.book.springboot.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
-import java.util.Collections;
 
 @RequiredArgsConstructor
 @Service
@@ -32,12 +27,5 @@ public class CustomOauth2UserService implements OAuth2UserService<OAuth2UserRequ
         String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
 
         OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttribute());
-    }
-
-    private User saveOrUpdate(OAuthAttributes attributes) {
-        User user =
-        userRepository.findByEmail(attributes.getEmail()).map(entity -> entity.update(attributes.getName(), attributes.getPicture())).orElse(attributes.toEntity());
-
-        return userRepository.save(user);
     }
 }
