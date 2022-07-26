330005cb403fa7a75d97f413a453d04a05b41259
liddle99 <liddle9985@gmail.com>
2022-07-26 14:50:14 +0900

    feat: 게시글 등록기능 추가

--- server/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ server/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -2,17 +2,13 @@ package com.jojoldu.book.springboot.web;
 
 import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.posts.PostsService;
-import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
-import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
-import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
