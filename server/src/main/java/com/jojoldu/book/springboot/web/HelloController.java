4375d92d26dcd57e320631c371420bb5aca8adff
liddle99 <liddle9985@gmail.com>
2022-08-05 10:43:19 +0900

    feat: MockMVC추가

--- server/src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ server/src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -7,13 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
 
     @GetMapping("/hello/dto")
-    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
         return new HelloResponseDto(name, amount);
     }
+
 }
